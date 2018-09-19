bc169b71a2f81c9a4330511926fc7e05ea2ccb03
hozuki <meta.maetel@gmail.com>
2018-09-19 17:38:05 +0900

    Playerのタグを修正

---
+++ Assets/Script/PlayerStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+namespace GameJam.Sho
+{
+    public class PlayerStatus : Status
+    {
+        [SerializeField]
+        private int windMax = 5;
+
+        public int WindMax => windMax;
+
+        private int currentWindCount = 0;
+        public int CurrentWindCount
+        {
+            get { return currentWindCount; }
+            set
+            {
+                currentWindCount = value;
+                currentWindCount = Mathf.Max(currentWindCount, 0);
+                _WindCountChangeEvent.OnNext(currentWindCount);
+            }
+        }
+
+        private ISubject<int> _WindCountChangeEvent { get; set; } = new Subject<int>();
+        public IObservable<int> WindCountChangeEvent => _WindCountChangeEvent;
+
+        // Use this for initialization
+        void Start()
+        {
+            CurrentWindCount = 0;
+        }
+    }
+}
--- Assets/Script/Status.cs
+++ Assets/Script/Status.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
 
 namespace GameJam.Sho
 {
     public class Status : MonoBehaviour
     {
+        [SerializeField]
+        private int hp = 10;
+        public int HP
+        {
+            get { return hp; }
+            set
+            {
+                hp = value;
+                hp = Mathf.Max(hp, 0);
+                if (hp == 0) _DeadEvent.OnNext(Unit.Default);
+                else _HpChangedEvent.OnNext(hp);
+            }
+        }
+
+        private ISubject<int> _HpChangedEvent { get; set; } = new Subject<int>();
+        public IObservable<int> HPChangedEvent => _HpChangedEvent;
+
+        private ISubject<Unit> _DeadEvent { get; set; } = new Subject<Unit>();
+        public IObservable<Unit> DeadEvent => _DeadEvent;
 
         // Use this for initialization
         void Start()
         {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            HP = hp;
         }
     }
 }
